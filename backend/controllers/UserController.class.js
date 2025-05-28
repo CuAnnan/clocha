44cccf5b580983a231435d2ca2d041706051d029
Wing <eamonn.kearns@so-4pt.net>
2025-05-28 16:41:53 +0100

    Logins system begun.

--- backend/controllers/UserController.class.js
+++ backend/controllers/UserController.class.js
@@ -13,6 +13,34 @@ class UserController extends Controller {
         super();
     }
 
+    confirmUserId(username, password)
+    {
+        const userNotFoundError = "Username or password incorrect";
+
+        return new Promise(async (resolve, reject)=>{
+            const user = await this.db.collection('users').findOne({username});
+            if(!user)
+            {
+                reject(userNotFoundError);1
+                return;
+            }
+            bcrypt.compare(password, user.passwordHash, (err, result) => {
+                if(result)
+                {
+                    resolve(user);
+                }
+                else
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    reject(userNotFoundError);
+                }
+            });
+        });
+    }
+
     async addUser(req, res) {
         let user = req.body;
         user.passwordHash  = await bcrypt.hash(req.body.password, SALT_ROUNDS);
@@ -23,7 +51,21 @@ class UserController extends Controller {
 
     async login(req, res)
     {
+        try
+        {
+            let user = await this.confirmUserId(req.body.username, req.body.password);
+            delete user.passwordHash;
+            const userToken = Object.assign({}, user);
 
+            const accessToken = jwt.sign(userToken,conf.express.jwt.secret,{expiresIn: '1m'});
+            const refreshToken = jwt.sign(userToken, conf.express.jwt.secret, {expiresIn: '1d'});
+            res.status(200).json({login: true, accessToken, refreshToken, user:userToken});
+        }
+        catch(err)
+        {
+            console.log(err);
+            res.status(401).json({login:false, errors:["Username or password incorrect"]});
+        }
     }
 
     static getInstance()
